25ab6d3a091db56e41fa8731313766cd86e2b7ab
junyorrf <rogeriojunyorfernandes@gmail.com>
2024-09-15 10:19:18 -0300

    feat: added role-based access control to restrict non-admin users

--- pages/create.php
+++ pages/create.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if (!isset($_SESSION['username'])) {
-    header('Location: ../index.php');
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
+    header('Location: ../index.php'); 
     exit();
 }
 ?>
